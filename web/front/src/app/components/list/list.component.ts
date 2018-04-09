203981493a9842213bd9493b22c2a02410f4f711
gu <1376295429@qq.com>
2018-04-09 21:24:00 +0800

    修改commit

--- web/front/src/app/components/list/list.component.ts
+++ web/front/src/app/components/list/list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
 
